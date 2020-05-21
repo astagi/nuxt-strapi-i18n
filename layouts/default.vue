53da798bff8833b4f40e616423a2f60094951a20
Andrea Stagi <stagi.andrea@gmail.com>
2020-05-22 00:09:16 +0200

    Fix navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,29 +1,19 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <nuxt-link
-          class="navbar-item"
-          :to="localePath({ name: 'index' })"
-        >
+    <b-navbar class="has-shadow is-primary">
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="localePath({ name: 'index' })">
           <img
             src="https://pngimg.com/uploads/jam/jam_PNG93.png"
             alt="Buefy"
             height="28"
           > <span class="blogtitle">JAMBlog</span>
-        </nuxt-link>
-      </div>
-      <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-          <language-switcher />
-        </div>
-      </div>
-    </nav>
-
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <language-switcher />
+      </template>
+    </b-navbar>
     <section class="main-content">
       <div class="container column is-12">
         <nuxt />
